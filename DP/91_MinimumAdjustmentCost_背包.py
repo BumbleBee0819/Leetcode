fd8b15453e148baea0fc89e4cedc29cfb52e0cbb
Wenyan Bi <wb1918a@student.american.edu>
2019-09-26 11:04:16 -0500

    Update 91_MinimumAdjustmentCost_背包.py

--- DP/91_MinimumAdjustmentCost_背包.py
+++ DP/91_MinimumAdjustmentCost_背包.py
@@ -37,6 +37,61 @@ class Solution:
                 for k in range(0, target+1):
                     if j-k < 0 and j + k >= len(DP[0]):
                         break
+                        
+                        
+                        
+# 空间优化
+class Solution1:
+    """
+    @param: A: An integer array
+    @param: target: An integer
+    @return: An integer
+    """
+    def MinAdjustmentCost(self, A, target):
+        # write your code here
+        if len(A) <= 1:
+            return 0
+        if target < 0:
+            return -1
+        
+            
+            
+        
+        # DP[i][j]: min cost for first i items & the i-th item' val is j
+        
+        DP = [[float('Inf')] * (8) for i in range(2)]
+        
+        # first row
+        for j in range(len(DP[0])):
+            DP[0][j] = abs(j - A[0])
+            
+
+        for i in range(1, len(A)):
+            thisA = A[i]
+            
+
+            
+            for j in range(len(DP[0])):
+                # 空间优化时一定要注意每个值每一步都需要更新，不能用default value
+                DP[i%2][j] = float('Inf')
+                thisB = j
+                thisAdjustment = abs(thisA-thisB)
+                
+                for k in range(target+1):
+                    if j-k < 0 and j + k >= len(DP[0]):
+                        break
+                    
+                    lastAdjustment = float('Inf')
+                    if j-k >= 0:
+                        lastAdjustment = min(lastAdjustment, DP[(i-1)%2][j-k])
+                    if j+k < len(DP[0]):
+                        lastAdjustment = min(lastAdjustment, DP[(i-1)%2][j+k])
+                        
+                    DP[i%2][j] = min(DP[i%2][j], lastAdjustment+thisAdjustment)
+            
+
+
+        return min(DP[i%2])
                     
                     lastAdjustment = float('Inf')
                     if j-k >= 0:
